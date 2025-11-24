4c0e0289ffb85879ee17fe860251e82c3bb90750
Elisavet Chatzikyrka <echatzikyrka@gmail.com>
2025-11-25 00:20:02 +0200

    implement file and pipe operations

--- tinyShell.c
+++ tinyShell.c
@@ -1,14 +1,16 @@
 /*
- * TinyShell - Phase 1
- * A Unix shell implementation
+ * TinyShell - Phase 2
+ * A Unix shell implementation with I/O redirection and pipes
  */
 
 #define _GNU_SOURCE
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
@@ -19,22 +21,154 @@
 #define COLOR_CYAN    "\033[1;36m"
 
 #define MAX_ARGS 128
+#define MAX_CMDS 64
 #define PATH_MAX_LEN 1024
 
 extern char **environ;
 
-// Simple parsing: separates the line into tokens (space/tab/newline)
-static int parse_line(char *line, char **argv) 
+// Forward declarations
+static void builtin_exit(int argc, char **argv);
+static void builtin_cd(int argc, char **argv);
+static void builtin_help(void);
+
+// Command structure for pipeline
+typedef struct {
+    char *argv[MAX_ARGS];  // Arguments for this command
+    int argc;               // Number of arguments
+    char *infile;          // Input redirection filename (NULL if none)
+    char *outfile;         // Output redirection filename (NULL if none)
+    char *errfile;         // Stderr redirection filename (NULL if none)
+    int append;            // 1 for >>, 0 for >
+} Command;
+
+// Parse a single command, checking for redirections (<, >, >>, 2>)
+static void parse_command(char *input, Command *cmd) 
 {
-    int argc = 0;
-    char *tok = strtok(line, " \t\r\n");
-    while (tok && argc < MAX_ARGS - 1) 
+    cmd->argc = 0;
+    cmd->infile = NULL;
+    cmd->outfile = NULL;
+    cmd->errfile = NULL;
+    cmd->append = 0;
+    
+    char *cmd_start = input;
+    char *pos = input;
+    
+    // Scan through input and extract all redirections
+    while (*pos) 
     {
-        argv[argc++] = tok;
-        tok = strtok(NULL, " \t\r\n");
+        if (pos[0] == '2' && pos[1] == '>') 
+        {
+            // Stderr redirection
+            *pos = '\0';
+            pos += 2;
+            while (*pos == ' ' || *pos == '\t') pos++;
+            
+            char *start = pos;
+            while (*pos && *pos != ' ' && *pos != '\t' && *pos != '<' && *pos != '>' && *pos != '\n') 
+                pos++;
+            
+            if (pos > start) 
+            {
+                cmd->errfile = start;
+                if (*pos) *pos++ = '\0';
+            }
+        }
+        else if (pos[0] == '>' && pos[1] == '>') 
+        {
+            // Append redirection
+            *pos = '\0';
+            pos += 2;
+            while (*pos == ' ' || *pos == '\t') pos++;
+            
+            char *start = pos;
+            while (*pos && *pos != ' ' && *pos != '\t' && *pos != '<' && *pos != '>' && *pos != '\n') 
+                pos++;
+            
+            if (pos > start) 
+            {
+                cmd->outfile = start;
+                cmd->append = 1;
+                if (*pos) *pos++ = '\0';
+            }
+        }
+        else if (*pos == '>') 
+        {
+            // Output redirection
+            *pos = '\0';
+            pos++;
+            while (*pos == ' ' || *pos == '\t') pos++;
+            
+            char *start = pos;
+            while (*pos && *pos != ' ' && *pos != '\t' && *pos != '<' && *pos != '>' && *pos != '\n') 
+                pos++;
+            
+            if (pos > start) 
+            {
+                cmd->outfile = start;
+                cmd->append = 0;
+                if (*pos) *pos++ = '\0';
+            }
+        }
+        else if (*pos == '<') 
+        {
+            // Input redirection
+            *pos = '\0';
+            pos++;
+            while (*pos == ' ' || *pos == '\t') pos++;
+            
+            char *start = pos;
+            while (*pos && *pos != ' ' && *pos != '\t' && *pos != '<' && *pos != '>' && *pos != '\n') 
+                pos++;
+            
+            if (pos > start) 
+            {
+                cmd->infile = start;
+                if (*pos) *pos++ = '\0';
+            }
+        }
+        else 
+        {
+            pos++;
+        }
     }
-    argv[argc] = NULL;
-    return argc;
+    
+    // Tokenize the command part (what's left after removing redirections)
+    char *token = strtok(cmd_start, " \t\n");
+    while (token && cmd->argc < MAX_ARGS - 1) 
+    {
+        if (*token) 
+        {
+            cmd->argv[cmd->argc++] = token;
+        }
+        token = strtok(NULL, " \t\n");
+    }
+    cmd->argv[cmd->argc] = NULL;
+}
+
+// Split input by | to get pipeline commands
+static int split_pipeline(char *input, Command cmds[]) 
+{
+    int num_cmds = 0;
+    char *saveptr;
+    char *cmd_str = strtok_r(input, "|", &saveptr);
+    
+    while (cmd_str && num_cmds < MAX_CMDS) 
+    {
+        // Skip leading whitespace
+        while (*cmd_str && (*cmd_str == ' ' || *cmd_str == '\t')) 
+            cmd_str++;
+        
+        if (*cmd_str) 
+        {
+            parse_command(cmd_str, &cmds[num_cmds]);
+            if (cmds[num_cmds].argc > 0) 
+                num_cmds++;
+        }
+        
+        cmd_str = strtok_r(NULL, "|", &saveptr);
+    }
+    
+    return num_cmds;
 }
 
 // Execution with manual PATH search + execve
@@ -95,30 +229,221 @@ static void print_exit_status(int status)
     }
 }
 
+// Set up input/output/error redirection if needed
+static void setup_redirection(Command *cmd) 
+{
+    // Input redirection (<)
+    if (cmd->infile) 
+    {
+        int fd = open(cmd->infile, O_RDONLY);
+        if (fd < 0) 
+        {
+            fprintf(stderr, "%s%s: %s%s\n", COLOR_RED, cmd->infile, strerror(errno), COLOR_RESET);
+            _exit(1);
+        }
+        
+        if (dup2(fd, STDIN_FILENO) < 0) 
+        {
+            perror("dup2");
+            close(fd);
+            _exit(1);
+        }
+        close(fd);
+    }
+    
+    // Output redirection (> or >>)
+    if (cmd->outfile) 
+    {
+        int flags = O_WRONLY | O_CREAT;
+        if (cmd->append) 
+            flags |= O_APPEND;
+        else 
+            flags |= O_TRUNC;
+        
+        int fd = open(cmd->outfile, flags, 0644);
+        if (fd < 0) 
+        {
+            perror("open");
+            _exit(1);
+        }
+        
+        if (dup2(fd, STDOUT_FILENO) < 0) 
+        {
+            perror("dup2");
+            close(fd);
+            _exit(1);
+        }
+        close(fd);
+    }
+    
+    // Stderr redirection (2>)
+    if (cmd->errfile) 
+    {
+        int fd = open(cmd->errfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd < 0) 
+        {
+            perror("open");
+            _exit(1);
+        }
+        
+        if (dup2(fd, STDERR_FILENO) < 0) 
+        {
+            perror("dup2");
+            close(fd);
+            _exit(1);
+        }
+        close(fd);
+    }
+}
+
+// Execute a pipeline of commands
+static void execute_pipeline(Command cmds[], int num_cmds) 
+{
+    if (num_cmds == 0) 
+        return;
+    
+    // Check for built-in commands (only valid for single command, no pipes, no redirections)
+    if (num_cmds == 1 && cmds[0].outfile == NULL && cmds[0].infile == NULL && cmds[0].errfile == NULL) 
+    {
+        if (strcmp(cmds[0].argv[0], "exit") == 0) 
+        {
+            builtin_exit(cmds[0].argc, cmds[0].argv);
+            return;
+        }
+        if (strcmp(cmds[0].argv[0], "cd") == 0) 
+        {
+            builtin_cd(cmds[0].argc, cmds[0].argv);
+            return;
+        }
+        if (strcmp(cmds[0].argv[0], "help") == 0) 
+        {
+            builtin_help();
+            return;
+        }
+    }
+    
+    // Single command (possibly with redirection)
+    if (num_cmds == 1) 
+    {
+        pid_t pid = fork();
+        if (pid < 0) 
+        {
+            perror("fork");
+            return;
+        }
+        
+        if (pid == 0) 
+        {
+            // Child process
+            setup_redirection(&cmds[0]);
+            exec_with_path(cmds[0].argv[0], cmds[0].argv);
+        } 
+        else 
+        {
+            // Parent process
+            int status;
+            waitpid(pid, &status, 0);
+            print_exit_status(status);
+        }
+        return;
+    }
+    
+    // Multiple commands in pipeline
+    int pipefds[2 * (num_cmds - 1)];
+    
+    // Create all pipes
+    for (int i = 0; i < num_cmds - 1; i++) 
+    {
+        if (pipe(pipefds + i * 2) < 0) 
+        {
+            perror("pipe");
+            return;
+        }
+    }
+    
+    // Fork and execute each command
+    pid_t pids[MAX_CMDS];
+    for (int i = 0; i < num_cmds; i++) 
+    {
+        pids[i] = fork();
+        if (pids[i] < 0) 
+        {
+            perror("fork");
+            return;
+        }
+        
+        if (pids[i] == 0) 
+        {
+            // Child process
+            
+            // Redirect input from previous pipe (if not first command)
+            if (i > 0) 
+            {
+                if (dup2(pipefds[(i - 1) * 2], STDIN_FILENO) < 0) 
+                {
+                    perror("dup2");
+                    _exit(1);
+                }
+            }
+            
+            // Redirect output to next pipe (if not last command)
+            if (i < num_cmds - 1) 
+            {
+                if (dup2(pipefds[i * 2 + 1], STDOUT_FILENO) < 0) 
+                {
+                    perror("dup2");
+                    _exit(1);
+                }
+            }
+            
+            // Close all pipe file descriptors
+            for (int j = 0; j < 2 * (num_cmds - 1); j++) 
+                close(pipefds[j]);
+            
+            // Set up output redirection if this is the last command
+            if (i == num_cmds - 1) 
+                setup_redirection(&cmds[i]);
+            
+            // Execute command
+            exec_with_path(cmds[i].argv[0], cmds[i].argv);
+        }
+    }
+    
+    // Parent process: close all pipes
+    for (int i = 0; i < 2 * (num_cmds - 1); i++) 
+        close(pipefds[i]);
+    
+    // Wait for all children
+    int status;
+    for (int i = 0; i < num_cmds; i++) 
+    {
+        waitpid(pids[i], &status, 0);
+        if (i == num_cmds - 1)  // Only print status of last command
+            print_exit_status(status);
+    }
+}
+
 // Built-in: exit command
-static int builtin_exit(int argc, char **argv, char *line)
+static void builtin_exit(int argc, char **argv)
 {
     int code = 0;
     if (argc >= 2) code = atoi(argv[1]);
-    free(line);
     exit(code);
 }
 
 // Built-in: cd command
-static int builtin_cd(int argc, char **argv)
+static void builtin_cd(int argc, char **argv)
 {
     const char *dir = (argc >= 2) ? argv[1] : getenv("HOME");
     if (!dir) 
     {
         fprintf(stderr, "%scd: HOME not set%s\n", COLOR_RED, COLOR_RESET);
-        return 1;
+        return;
     }
     if (chdir(dir) != 0) 
     {
         perror("cd");
-        return 1;
     }
-    return 0;
 }
 
 // Built-in: help command
@@ -161,53 +486,24 @@ int main(void)
         // Ignore empty lines
         if (n <= 1) continue;
 
-        // Parsing
-        char *argv[MAX_ARGS];
-        int argc = parse_line(line, argv);
-        if (argc == 0) continue;
-
-        // Check for built-in commands
-        if (strcmp(argv[0], "exit") == 0) 
+        // Make a copy of line for parsing (since strtok modifies it)
+        char *line_copy = strdup(line);
+        if (!line_copy) 
         {
-            builtin_exit(argc, argv, line);
-        }
-
-        if (strcmp(argv[0], "cd") == 0) 
-        {
-            builtin_cd(argc, argv);
+            perror("strdup");
             continue;
         }
 
-        if (strcmp(argv[0], "help") == 0) 
+        // Parse pipeline
+        Command cmds[MAX_CMDS];
+        int num_cmds = split_pipeline(line_copy, cmds);
+        
+        if (num_cmds > 0) 
         {
-            builtin_help();
-            continue;
+            execute_pipeline(cmds, num_cmds);
         }
 
-        // Fork + execve
-        pid_t pid = fork();
-        if (pid < 0) 
-        {
-            perror("fork");
-            continue;
-        }
-        if (pid == 0) 
-        {
-            // child: PATH search + execve
-            exec_with_path(argv[0], argv);
-            _exit(127); // safety
-        } 
-        else 
-        {
-            // parent: Wait and report status
-            int status;
-            if (waitpid(pid, &status, 0) == -1) 
-            {
-                perror("waitpid");
-                continue;
-            }
-            print_exit_status(status);
-        }
+        free(line_copy);
     }
 
     free(line);
